c811060f337678ed340dfcc77d94bff66e7760b9
AmitAgnihotri <agni.amit81@gmail.com>
2024-03-12 15:04:02 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Amit Agnihotri - Welcome to Jenkins CICD World!'
+    return 'Hello, Amit Agnihotri - Welcome to Jenkins CICD World! Happy Learning'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
